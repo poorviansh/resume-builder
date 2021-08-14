32f7d0bcf1570edd82c17f0b9639f902006c3544
Poorvi Chaudhary <poorviansh@gmail.com>
2021-08-15 01:25:40 +0530

    Changed logs

--- src/components/presentation/aboutUs.js
+++ src/components/presentation/aboutUs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import aboutUs from "../../static/images/aboutus.jpg";
 
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 const AboutUs = () => {
     
     return (        
